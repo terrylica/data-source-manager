5f51dd0030c110d77b598e53b1af307434eb41bf
terrylica <terry@eonlabs.com>
2025-04-22 19:45:09 +0000

    Add another test file with unused imports cleaned up

---
+++ test_unused_imports2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Another test file for autoflake pre-commit hook."""
+
+
+
+def hello():
+    """Say hello."""
+    return "Hello, world!"
+
+
+if __name__ == "__main__":
+    print(hello())
